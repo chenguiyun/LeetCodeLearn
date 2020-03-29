cd0403ee2c2eb87e4f832dad1b3dae8f9f4dcbb6
cgy <1034682006@qq.com>
2020-03-30 03:21:46 +0800

    随机题目

--- ConsoleLeetCode/Program.cs
+++ ConsoleLeetCode/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleLeetCode
 {
@@ -7,7 +9,10 @@ namespace ConsoleLeetCode
         static void Main(string[] args)
         {
 
-            var re = Reverse(321);
+            //var re = Reverse(321);
+            char[] te = "aabbcc".ToArray();
+            int[] nums = { 3, 10, 5, 25, 2, 8 };
+            int re = FindMaximumXOR(nums);
             Console.WriteLine(re);
             Console.ReadKey();
 
@@ -45,6 +50,106 @@ namespace ConsoleLeetCode
             {
                 return 0;
             }
+        }
+
+        public static int Compress(char[] chars)
+        {
+            var li = new List<string>();
+            foreach (var c in chars)
+            {
+                li.Add(c.ToString());
+            }
+            var oli = li.ToList<string>();
+            var reli = li.Distinct().ToList<string>();
+            var li1 = new List<string>();
+            var li2 = new List<string>();
+            if (reli.Count <= 1) {
+                return 1;
+            }
+            foreach (var r in reli)
+            {
+                li1.Add(r);
+                var o = oli.Count(f => f.Equals(r));
+                if (o > 1 && o < 10)
+                {
+                    li2.Add(o.ToString());
+                }
+                else if (o >= 0 && o < 100)
+                {
+                    li2.Add(o.ToString().ToCharArray()[0].ToString());
+                    li2.Add(o.ToString().ToCharArray()[1].ToString());
+                }
+                else if (o >= 100 && o < 1000)
+                {
+                    li2.Add(o.ToString().ToCharArray()[0].ToString());
+                    li2.Add(o.ToString().ToCharArray()[1].ToString());
+                    li2.Add(o.ToString().ToCharArray()[2].ToString());
+                }
+                else 
+                {
+                    li2.Add(o.ToString().ToCharArray()[0].ToString());
+                    li2.Add(o.ToString().ToCharArray()[1].ToString());
+                    li2.Add(o.ToString().ToCharArray()[2].ToString());
+                    li2.Add(o.ToString().ToCharArray()[3].ToString());
+                }
+            }
+            var re = li1.Count + li2.Count;
+            var s = "";
+            foreach (var l in li1)
+            {
+                s = s + l;
+            }
+            foreach (var l in li2)
+            {
+                s = s + l;
+            }
+            chars = s.ToCharArray();
+            var rs = "[";
+            for (int i = 0; i < chars.Length; i++)
+            {
+                var end = ",";
+                if (i == chars.Length - 1)
+                {
+                    end = "";
+                }
+                rs = rs + '"' + chars[i] + '"' + end;
+            }
+            rs = rs + "]";
+            Console.Write(rs);
+            return chars.Length;
+        }
+
+        public static int FindMaximumXOR(int[] nums)
+        {
+            var li = new List<int>();
+            int i = 0;
+            int j = 0;
+            var re = new int[nums.Length * nums.Length];
+            while (i < nums.Length && j < nums.Length)
+            {
+                var r = nums[i] ^ nums[j];
+                if (i == j)
+                {
+                    if (i == 0 || i == nums.Length - 1)
+                    {
+                        re[(i) * (j)] = r;
+                    }
+
+                }
+                else
+                {
+                    re[(i + 1) * (j + 1)] = r;
+                }
+                if (i == nums.Length - 1)
+                {
+                    i = 0;
+                    j++;
+                }
+                else {
+                    i++;
+                }
+            }
+            return re.Max();
         }
     }
 }
